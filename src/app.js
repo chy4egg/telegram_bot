57d7c1f982c3a8d327f959f8ee07091b87d9c5d5
Aleksandr <chy4egg@gmail.com>
2017-10-27 23:26:20 +0300

    tmp save

--- src/app.js
+++ src/app.js
@@ -1,36 +1,50 @@
-
 var tress = require('tress');
 var needle = require('needle');
+var cheerio = require('cheerio');
+var resolve = require('url').resolve;
+var fs = require('fs');
 
+// var URL = 'http://www.ferra.ru/ru/techlife/news/';
 var URL = 'https://pitercss.timepad.ru/events/';
 var results = [];
 
-// `tress` последовательно вызывает наш обработчик для каждой ссылки в очереди
 var q = tress(function(url, callback){
-
-    //тут мы обрабатываем страницу с адресом url
     needle.get(url, function(err, res){
         if (err) throw err;
 
-        // здесь делаем парсинг страницы из res.body
-            // делаем results.push для данных о новости
-            // делаем q.push для ссылок на обработку
+        // парсим DOM
+        var $ = cheerio.load(res.body);
+        
+    console.log(res.body); // native html code of the page ! yeah
+        
+        //информация о новости
+        if($('.b_infopost').contents().eq(2).text().trim().slice(0, -1) === 'Алексей Козлов'){
+            results.push({
+                title: $('h1').text(),
+                date: $('.b_infopost>.date').text(),
+                href: url,
+                size: $('.newsbody').text().length
+            });
+        }
 
-        callback(); //вызываем callback в конце
+        //список новостей
+        $('.b_rewiev p>a').each(function() {
+            q.push($(this).attr('href'));
+        });
+
+        //паджинатор
+        $('.bpr_next>a').each(function() {
+            // не забываем привести относительный адрес ссылки к абсолютному
+            q.push(resolve(URL, $(this).attr('href')));
+        });
+
+        callback();
     });
-});
+}, 10); // запускаем 10 параллельных потоков
 
-// эта функция выполнится, когда в очереди закончатся ссылки
+
 q.drain = function(){
-    require('fs').writeFileSync('./data.json', JSON.stringify(results, null, 4));
-}
+    fs.writeFileSync('./data.json', JSON.stringify(results, null, 4));
+};
 
-// добавляем в очередь ссылку на первую страницу списка
 q.push(URL);
-
-
-// needle.get(URL, function (err, res, body) {
-//     if (err) throw err;
-//     console.log(body);
-//     console.log(res.statusCode);
-// });
